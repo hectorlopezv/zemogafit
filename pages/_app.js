92ba18a8bc08b78b367bf3282c083db3acfff54c
hvlopez <hectorvmlopez@gmail.com>
2022-01-24 03:50:19 -0500

    adding open graph support

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.scss'
 import { RecoilRoot } from 'recoil'
 import Head from 'next/head'
-if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+if (process.env.NEXT_PUBLIC_REQUEST_MOCK === 'enabled') {
   require('../testUtils/mswEntry')
 }
 function MyApp({ Component, pageProps }) {
@@ -10,9 +10,11 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="zemoga thumbsup fit" />
-
+        <meta name="og:description" content="zemoga thumbsup fit" />
+        <meta property="og:title" content="zemogafit rule of thumb" />
+        <meta property="og:type" content="website" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta property="og:image" content="https://pbs.twimg.com/profile_images/1411138632/ZemogaLogo__400x400.png" />
         <title>Rule of Thumb</title>
       </Head>
       <Component {...pageProps} />
